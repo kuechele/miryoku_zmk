aa6291615ed63de2009ad43804c3557f34d64d3c
kuechele <linus.kuch@web.de>
2024-04-04 19:08:25 +0200

    Refined custom_config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,16 +1,4 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-# Uncomment the following line to increase the keyboard's wireless range
-CONFIG_BT_CTLR_TX_PWR_PLUS_8=y
-
-# Enable eager debouncing
-CONFIG_ZMK_KSCAN_DEBOUNCE_PRESS_MS=1
-CONFIG_ZMK_KSCAN_DEBOUNCE_RELEASE_MS=7
-
-# Uncomment the following lines to enable the Corne RGB Underglow
-CONFIG_ZMK_RGB_UNDERGLOW=y
-CONFIG_WS2812_STRIP=y
-CONFIG_ZMK_RGB_UNDERGLOW_EFF_START=1
-
-CONFIG_ZMK_KEYBOARD_NAME="yeehhaa"
+#define MIRYOKU_EXTRA_QWERTZ
